bcb5d0b47665f803915341ad469f5953aa237aa7
mahdyar-developer <155878191+mahdyar-developer@users.noreply.github.com>
2024-01-27 07:06:47 -0800

    Update index.js

--- home-page/index.js
+++ home-page/index.js
@@ -1,14 +1,26 @@
 const img =document.querySelectorAll('.img')
 const parentclick =document.querySelector('#parentclick')
+let point =10
+const clid =document.querySelector('.texts')
+clid.addEventListener('click', () =>{
+   document.querySelector('.point').textContent=10
+})
 parentclick.addEventListener("click",(i)=>{
     const imge =document.querySelector(`.img-${Math.trunc(Math.random()*3)+1}`)
-    imge.classList.add('hidden')
     imge.getAttribute("alt")
     img.forEach((w) => {w.classList.add('hidden')});
      const targetclick =i.target.closest('.imge').getAttribute("alt")
+     imge.classList.remove('hidden')
      
-        
-        if (targetclick===imge.getAttribute("alt")) {
-            console.log('hello')
-        }
+  
+     if (targetclick===imge.getAttribute("alt")) { }
+     else{
+        if (targetclick==='w') {(imge.getAttribute("alt")==='f')? point--: point++}
+        else if (targetclick==='e') {(imge.getAttribute("alt")==='f')? point++ : point--}
+        else if (targetclick==='f') {(imge.getAttribute("alt")==='e')? point-- : point++}
+     }    
+     
+     document.querySelector('.point').textContent=point
+
+     
 })
