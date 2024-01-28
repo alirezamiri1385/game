aab795b59ffa677221618aea2294a751f5d1992d
mahdyar-developer <155878191+mahdyar-developer@users.noreply.github.com>
2024-01-28 08:08:52 -0800

    Update index.js

--- home-page/index.js
+++ home-page/index.js
@@ -1,26 +1,17 @@
 const img =document.querySelectorAll('.img')
 const parentclick =document.querySelector('#parentclick')
 let point =10
-const clid =document.querySelector('.texts')
-clid.addEventListener('click', () =>{
-   document.querySelector('.point').textContent=10
-})
 parentclick.addEventListener("click",(i)=>{
     const imge =document.querySelector(`.img-${Math.trunc(Math.random()*3)+1}`)
     imge.getAttribute("alt")
     img.forEach((w) => {w.classList.add('hidden')});
      const targetclick =i.target.closest('.imge').getAttribute("alt")
      imge.classList.remove('hidden')
-     
-  
-     if (targetclick===imge.getAttribute("alt")) { }
+     if (targetclick===imge.getAttribute("alt")) {(document.querySelector('body').style.backgroundColor="yellow")}
      else{
-        if (targetclick==='w') {(imge.getAttribute("alt")==='f')? point--: point++}
-        else if (targetclick==='e') {(imge.getAttribute("alt")==='f')? point++ : point--}
-        else if (targetclick==='f') {(imge.getAttribute("alt")==='e')? point-- : point++}
+        if (targetclick==='w') {(imge.getAttribute("alt")==='f')? point--&&( document.querySelector('body').style.backgroundColor="red"): point++&&( document.querySelector('body').style.backgroundColor="green")}
+        else if (targetclick==='e') {(imge.getAttribute("alt")==='f')? point++&&( document.querySelector('body').style.backgroundColor="green") : point--&&( document.querySelector('body').style.backgroundColor="red")}
+        else if (targetclick==='f') {(imge.getAttribute("alt")==='e')? point--&&( document.querySelector('body').style.backgroundColor="red"): point++&&( document.querySelector('body').style.backgroundColor="green")}
      }    
-     
      document.querySelector('.point').textContent=point
-
-     
 })
